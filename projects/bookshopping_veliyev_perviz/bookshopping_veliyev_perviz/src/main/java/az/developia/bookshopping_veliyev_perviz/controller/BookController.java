638f36ead7ed6283f974b7fc7119e34e5f5c5e10
Perviz290 <112194956+Perviz290@users.noreply.github.com>
2022-11-30 08:30:42 -0800

    Book Delete Part1

--- projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/controller/BookController.java
+++ projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import az.developia.bookshopping_veliyev_perviz.dao.BookDAO;
@@ -45,8 +46,14 @@ public class BookController {
 	
 	
 	
-	
-	
+	@GetMapping(path = "/books/delete/{id}") 
+	public String editBook(@PathVariable(name = "id")Integer id,Model model) {
+		Book book=new Book();
+		model.addAttribute("book", book);
+		System.out.println(id);
+		
+		return "new-book";
+	}
 	
 	
 	
