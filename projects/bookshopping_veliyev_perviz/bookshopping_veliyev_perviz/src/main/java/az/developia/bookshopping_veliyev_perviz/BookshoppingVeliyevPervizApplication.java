b9178851ef4b0730f63a0f2624022e0eaf5eb1d2
Perviz290 <112194956+Perviz290@users.noreply.github.com>
2022-11-30 03:56:29 -0800

    spring project created

---
+++ projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/BookshoppingVeliyevPervizApplication.java
@@ -0,0 +1,13 @@
+package az.developia.bookshopping_veliyev_perviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshoppingVeliyevPervizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshoppingVeliyevPervizApplication.class, args);
+	}
+
+}
---
+++ projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/test/java/az/developia/bookshopping_veliyev_perviz/BookshoppingVeliyevPervizApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.bookshopping_veliyev_perviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshoppingVeliyevPervizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
