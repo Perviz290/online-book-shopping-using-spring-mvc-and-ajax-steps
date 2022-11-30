8ff4471e74b355351937753833ff9d86e0587469
Perviz290 <112194956+Perviz290@users.noreply.github.com>
2022-11-30 07:44:55 -0800

    Page-lere gecid linkleri elave etmek \

--- projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/controller/HomeController.java
+++ projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/controller/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping(value = "/home")
+	@GetMapping(value = {"/home","/"})
 	public String showHomePage() {
 		return "home";
 	}
+	
+	
+	
+	
+	
 }
