1a9351686f84cb7bab43d9661257f5d2da749efd
Perviz290 <112194956+Perviz290@users.noreply.github.com>
2022-12-05 04:43:55 -0800

    Alici sehfesinde kitab melumatlarini gostermek

--- projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/controller/CustomerController.java
+++ projects/bookshopping_veliyev_perviz/bookshopping_veliyev_perviz/src/main/java/az/developia/bookshopping_veliyev_perviz/controller/CustomerController.java
@@ -1,26 +1,46 @@
 package az.developia.bookshopping_veliyev_perviz.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import az.developia.bookshopping_veliyev_perviz.dao.BookDAO;
+import az.developia.bookshopping_veliyev_perviz.model.Book;
+
 
 @Controller
 public class CustomerController {
+	
+	@Autowired
+	BookDAO bookDAO;
+	
+	
 
-	@GetMapping(path = "/customer")  
+	@GetMapping(path = "/customer")  //Bazadan melumati goturub. thymeleaf fastesi ile alici sehfesinde gostermek 
 	public String showCustomerPage(Model model) {
-		ArrayList<String>books=new ArrayList<>();
-		for (int i = 1; i <=100; i++) {
-			books.add("");
-			}
+		List<Book>books=bookDAO.findAll();
 		model.addAttribute("books", books);
+			
 		return "customer";
-	} 
+	} 	
 	
 	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
